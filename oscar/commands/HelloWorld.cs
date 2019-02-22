2f2eb242f0baf246ad25b5ebca41f8754aa3b016
Jasper den Brok <j.g.b.denbrok@students.uu.nl>
2019-02-22 19:58:53 +0100

    Add hello world command

---
+++ oscar/commands/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace SansBot.Commands
+{
+    public class HelloWorld : ModuleBase<SocketCommandContext>
+    {
+        [Command("helloworld"), Alias("hw"), Summary("Bot posts the phrase 'Hello World!' in chat")]
+        public async Task HelloWorldAsync()
+        {
+            await Context.Channel.SendMessageAsync("Hello World!");
+            return;
+        }
+    }
+}
